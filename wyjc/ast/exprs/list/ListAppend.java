114c43257b14a6c9a1c4c941c799d5235cf2541e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-06-26 23:59:54 +0000

    Fixed list append.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@3858 cab769db-1af3-0310-8bf3-f2d376eea66d

--- wyjc/ast/exprs/list/ListAppend.java
+++ wyjc/ast/exprs/list/ListAppend.java
@@ -28,6 +28,7 @@ import wyjc.ast.exprs.*;
 import wyjc.ast.exprs.integer.IntVal;
 import wyjc.ast.types.ListType;
 import wyjc.ast.types.Type;
+import wyjc.ast.types.Types;
 import wyjc.util.Pair;
 import wyjc.util.ResolveError;
 import wyjc.util.Triple;
@@ -41,43 +42,34 @@ import static wyone.theory.logic.WFormulas.*;
 import static wyone.theory.numeric.WNumerics.*;
 
 
-public final class ListAppend extends SyntacticElementImpl implements Expr {
-	private Expr source;
-	private Expr start;	
-	private Expr end;
-	
-	public ListAppend(Expr source, Expr start, Expr end, Attribute... attributes)  {
-		super(attributes);
-		this.source = source;
-		this.start = start;
-		this.end = end;
+public final class ListAppend extends BinOp<Expr> {	
+	public ListAppend(Expr lhs, Expr rhs, Attribute... attributes)  {
+		super(lhs, rhs, null,attributes);		
 	}
 	
-	public ListAppend(Expr source, Expr start, Expr end, Collection<Attribute> attributes)  {
-		super(attributes);
-		this.source = source;		
-		this.start = start;
-		this.end = end;
+	public ListAppend(Expr lhs, Expr rhs, Collection<Attribute> attributes)  {
+		super(lhs, rhs, null,attributes);		
 	}
 	
 	public Type type(Map<String,Type> environment) {
-		Type src = source.type(environment).flattern();		
-		if(src instanceof ListType) {
-			return src;
+		Type _lhs_t = lhs.type(environment);
+		Type _rhs_t = rhs.type(environment);
+		if(!(_lhs_t instanceof ListType)) {
+			syntaxError("expecting list type",lhs);	
 		}
-		syntaxError("expecting list type",source);
-		return null;
+		if(!(_rhs_t instanceof ListType)) {
+			syntaxError("expecting list type",rhs);	
+		}
+		ListType lhs_t = (ListType) _lhs_t;
+		ListType rhs_t = (ListType) _rhs_t;
+		Type elem_t = Types.leastUpperBound(lhs_t.element(), rhs_t.element());
+		return new ListType(elem_t);
 	}
 	
-	public Expr source() { return source; }
-	public Expr start() { return start; }
-	public Expr end() { return end; }
-		
     public Expr substitute(Map<String,Expr> binding) {
-		Expr s = source.substitute(binding);
-		Expr i = start.substitute(binding);
-		Expr j = end.substitute(binding);
-		return new ListAppend(s,i,j,attributes());
+		Expr l = lhs.substitute(binding);
+		Expr r = rhs.substitute(binding);		
+		return new ListAppend(l,r,attributes());
 	}
     
     public Expr replace(Map<Expr,Expr> binding) {
@@ -85,100 +77,45 @@ public final class ListAppend extends SyntacticElementImpl implements Expr {
 		if(t != null) {
 			return t;
 		} else {
-			Expr l = source.replace(binding);
-			Expr s = start.replace(binding);
-			Expr e = end.replace(binding);
-			return new ListAppend(l,s,e,attributes());
+			Expr l = lhs.replace(binding);
+			Expr r = rhs.replace(binding);			
+			return new ListAppend(l,r,attributes());
 		}
 	}
-    
-    public <T> List<T> match(Class<T> match) {
-		List<T> matches = source.match(match);
-		matches.addAll(start.match(match));
-		if(match.isInstance(this)) {
-			matches.add((T)this);
-		}
-		return matches;
-	}
-	    
+    	    
     public Expr reduce(Map<String, Type> environment) {
-		Expr s = source.reduce(environment);
-		Expr e = start.reduce(environment);
-		Expr n = end.reduce(environment);
+		Expr l = lhs.reduce(environment);
+		Expr r = rhs.reduce(environment);		
 		
-		if (s instanceof ListVal && e instanceof IntVal && n instanceof IntVal) {
-			ListVal l = (ListVal) s;
-			IntVal i = (IntVal) e;
-			IntVal j = (IntVal) n;
-
-			if (i.value().compareTo(BigInteger.ZERO) < 0
-					|| i.value().compareTo(
-							BigInteger.valueOf(l.getValues().size())) >= 0) {
-				// no further reduction possible --- out of bounds
-				return new ListAppend(s, e, n, attributes());
-			} else if (j.value().compareTo(BigInteger.ZERO) < 0
-					|| j.value().compareTo(
-							BigInteger.valueOf(l.getValues().size())) >= 0) {
-				// no further reduction possible --- out of bounds
-				return new ListAppend(s, e, n, attributes());
-			} else if(i.value().compareTo(j.value()) > 0) {
-				// no further reduction possible --- i > j
-				return new ListAppend(s, e, n, attributes());
-			}
-			return new ListVal(l.getValues().subList(i.value().intValue(), j.value().intValue()));			
+		if (lhs instanceof ListVal && rhs instanceof ListVal) {
+			ListVal lv = (ListVal) l;
+			ListVal rv = (ListVal) r;
+			List<Value> lvs = lv.getValues();
+			List<Value> rvs = rv.getValues();
+			ArrayList<Value> nvs = new ArrayList<Value>(lvs);
+			nvs.addAll(rvs);			
+			return new ListVal(nvs,attributes());
 		} else {
 			// no further reduction possible.
-			return new ListAppend(s, e, n, attributes());
+			return new ListAppend(l,r, attributes());
 		}
 	}
-    
-    public Set<Variable> uses() {
-		Set<Variable> r = source.uses();
-		r.addAll(start.uses());
-		r.addAll(end.uses());
-		return r;
-	}
-    
+        
     public Triple<WExpr, WFormula, WEnvironment> convert(Map<String, Type> environment, ModuleLoader loader) throws ResolveError {		
-		Triple<WExpr, WFormula, WEnvironment> src = source.convert(environment, loader);
-		Triple<WExpr, WFormula, WEnvironment> start = this.start.convert(environment, loader);
-		Triple<WExpr, WFormula, WEnvironment> end = this.end.convert(environment, loader);
+		Triple<WExpr, WFormula, WEnvironment> l = lhs.convert(environment, loader);
+		Triple<WExpr, WFormula, WEnvironment> r = rhs.convert(environment, loader);		
 		
-		WEnvironment wenv = src.third();
-		wenv.addAll(start.third());
-		wenv.addAll(end.third());
+		WEnvironment wenv = l.third();
+		wenv.addAll(r.third());		
 		WVariable retVar = WVariable.freshVar();
 		wenv.add(retVar.name(), type(environment).convert());
+		WFormula constraints = and(l.second(), r.second());
 		
-		// first, identify new length					
-		WFormula lenConstraints = WExprs.equals(new WLengthOf(retVar), subtract(end
-				.first(), start.first()));
-				
-		// second, pump from src into retVar
-		WVariable i = WVariable.freshVar();
-		HashMap<WVariable,WExpr> variables = new HashMap();
-		variables.put(i,src.first());
-		WFormula lhs = WFormulas.and(WNumerics.lessThanEq(start.first(), i),
-				WNumerics.lessThan(i, end.first()));
-		WFormula rhs = WExprs.equals(new WListAccess(src.first(), i),
-				new WListAccess(retVar, WNumerics.subtract(i, start.first())));
-		WFormula forall1 = new WBoundedForall(true,variables,WFormulas.implies(lhs,rhs));
-		
-		// third, pump from retVar into src
-		
-		variables = new HashMap();
-		variables.put(i,retVar);
-		rhs = WExprs.equals(new WListAccess(src.first(), WNumerics.add(i,start.first())),
-				new WListAccess(retVar, i));
-		WFormula forall2 = new WBoundedForall(true,variables,rhs);
-		
-		WFormula constraints = and(start.second(), end.second(), src
-				.second(), lenConstraints, forall1, forall2);
 		return new Triple<WExpr, WFormula, WEnvironment>(retVar, constraints,
 				wenv);
 	}
     
     public String toString() {
-    	return source + "[" + start + ":" + end + "]";
+    	return lhs + " + " + rhs;
     }
 }
