c281401a29f1824efab496cc7ea83cab0f88fa24
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-07-19 20:32:35 +0000

    Added another test case to capture the more challenging case for recursive types.  This is the case that requires some kind of structural induction.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@4057 cab769db-1af3-0310-8bf3-f2d376eea66d

--- wyjc/testing/tests/DefiniteRuntimeInvalidTests.java
+++ wyjc/testing/tests/DefiniteRuntimeInvalidTests.java
@@ -69,6 +69,7 @@ public class DefiniteRuntimeInvalidTests extends TestHarness {
  @Test public void RecursiveType_Invalid_5_RunTest() { runtimeFailTest("RecursiveType_Invalid_5"); }
  @Test public void RecursiveType_Invalid_6_RunTest() { runtimeFailTest("RecursiveType_Invalid_6"); }
  @Test public void RecursiveType_Invalid_7_RunTest() { runtimeFailTest("RecursiveType_Invalid_7"); }
+ @Test public void RecursiveType_Invalid_8_RunTest() { runtimeFailTest("RecursiveType_Invalid_8"); }
  @Test public void Requires_Invalid_1_RunTest() { runtimeFailTest("Requires_Invalid_1"); }
  @Test public void SetAssign_Invalid_1_RunTest() { runtimeFailTest("SetAssign_Invalid_1"); }
  @Test public void SetElemOf_Invalid_1_RunTest() { runtimeFailTest("SetElemOf_Invalid_1"); } 
