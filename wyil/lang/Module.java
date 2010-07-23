280b99d4f8b8c8f0bcd1851ef00905ea9275c8e8
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-07-23 04:46:53 +0000

    Minor
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@4081 cab769db-1af3-0310-8bf3-f2d376eea66d

---
+++ wyil/lang/Module.java
@@ -0,0 +1,12 @@
+package wyil.lang;
+
+public abstract class Module {
+	
+	public final static class Function extends Module {
+		
+	}
+	
+	public final static class Define extends Module {
+		
+	}
+}
