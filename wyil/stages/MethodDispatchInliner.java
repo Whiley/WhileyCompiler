0683241e73f0e628ba6aee2f47f636f41d6bd7a7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-08-01 06:17:51 +0000

    Thinking thinking.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@4175 cab769db-1af3-0310-8bf3-f2d376eea66d

---
+++ wyil/stages/MethodDispatchInliner.java
@@ -0,0 +1,13 @@
+package wyil.stages;
+
+/**
+ * The purpose of the method dispatch inliner is to inline dispatch choices into
+ * call-sites. This offers a useful optimisation in situations when we can
+ * statically determine that a subset of cases is the dispatch target.
+ * 
+ * @author djp
+ * 
+ */
+public class MethodDispatchInliner {
+
+}
