6a9eb1dee92c824c72a81aa39b19c85e9024c551
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-09-25 01:01:20 +0000

    Updated missing licenses.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@4568 cab769db-1af3-0310-8bf3-f2d376eea66d

--- wyjvm/io/BytecodeFileWriter.java
+++ wyjvm/io/BytecodeFileWriter.java
@@ -1,23 +1,19 @@
-// This file is part of the Java Compiler Kit (JKit)
+// This file is part of the Wyjvm bytecode manipulation library.
 //
-// The Java Compiler Kit is free software; you can 
-// redistribute it and/or modify it under the terms of the 
-// GNU General Public License as published by the Free Software 
-// Foundation; either version 2 of the License, or (at your 
-// option) any later version.
+// Wyjvm is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; either version 3 of the License, 
+// or (at your option) any later version.
 //
-// The Java Compiler Kit is distributed in the hope
-// that it will be useful, but WITHOUT ANY WARRANTY; without 
-// even the implied warranty of MERCHANTABILITY or FITNESS FOR 
-// A PARTICULAR PURPOSE.  See the GNU General Public License 
-// for more details.
+// Wyjvm is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+// the GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public 
-// License along with the Java Compiler Kit; if not, 
-// write to the Free Software Foundation, Inc., 59 Temple Place, 
-// Suite 330, Boston, MA  02111-1307  USA
+// License along with Wyjvm. If not, see <http://www.gnu.org/licenses/>
 //
-// (C) David James Pearce, 2009. 
+// Copyright 2010, David James Pearce. 
 
 package wyjvm.io;
 
