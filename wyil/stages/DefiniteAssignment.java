0f92a85767bcfb223122f7b2bd0d852ef5a62dfa
DavePearce <david.pearce@ecs.vuw.ac.nz>
2010-10-02 05:04:18 +0000

    Almost reworked definied assignment to use the new ForwardFlowAnalysis class.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/u/staff/djp/projects/repository/whiley/tools/wyjc@4601 cab769db-1af3-0310-8bf3-f2d376eea66d

--- wyil/stages/DefiniteAssignment.java
+++ wyil/stages/DefiniteAssignment.java
@@ -45,44 +45,28 @@ import static wyil.util.SyntaxError.*;
  * 
  */
 public class DefiniteAssignment extends
-		ForwardAnalysis<IntersectionFlowSet<String>> implements ModuleTransform {
+		ForwardFlowAnalysis<HashSet<String>> implements ModuleTransform {
 	private String filename;
-	
-	public Module apply(Module module) {
-		filename = module.filename();
-		for(Module.Method method : module.methods()) {
-			check(method);
-		}
-		return module;
-	}
-	
-	public void check(Module.Method method) {
-		for(Module.Case cas : method.cases()) {
-			check(cas,method);
-		}
-	}
-	
-	public void check(Module.Case cas,Module.Method method) {
-		// the undefined set contains the names of all variables and registers
-		// which are defined at any given point.
+			
+	public HashSet<String> initialStore() {
 		HashSet<String> defined = new HashSet<String>();
 		
-		for(String p : cas.parameterNames()) {
+		for(String p : methodCase.parameterNames()) {
 			defined.add(p);
 		}
-	
+		
 		if(method.type().receiver != null) {
 			defined.add("this");
 		}
 		
-		start(cas, new IntersectionFlowSet<String>(defined),
-				new IntersectionFlowSet<String>());
+		return defined;
 	}
 	
-	public IntersectionFlowSet<String> transfer(Stmt stmt, IntersectionFlowSet<String> in) {		
+	public Pair<Stmt,HashSet<String>> propagate(Stmt stmt, HashSet<String> in) {		
 		Code code = stmt.code;
 		HashSet<String> uses = Code.usedVariables(code);		
 		String nvar = null;
+		
 		if(code instanceof Code.Assign) {
 			Code.Assign ca = (Code.Assign) code;			
 			if(ca.lhs instanceof CExpr.Variable) {
@@ -94,17 +78,7 @@ public class DefiniteAssignment extends
 				uses.remove("%" + v.index);				
 				nvar = "%" + v.index;
 			} 
-		} else if(code instanceof Code.Forall) {
-			Code.Forall ca = (Code.Forall) code;			
-			CExpr.Register v = (CExpr.Register) ca.variable;				
-			uses.remove("%" + v.index);				
-			nvar = "%" + v.index;			
-		} else if(code instanceof Code.Induct) {
-			Code.Induct ca = (Code.Induct) code;			
-			CExpr.Register v = (CExpr.Register) ca.variable;				
-			uses.remove("%" + v.index);				
-			nvar = "%" + v.index;			
-		}		
+		} 	
 		
 		// FIXME: there is a bug here for the value of a variable after a forall
 		// or induction block. The bug is that variables defined by these
@@ -114,14 +88,15 @@ public class DefiniteAssignment extends
 		checkUses(uses,in,stmt);
 		
 		if(nvar != null) {
-			in = in.add(nvar);
+			in = new HashSet<String>(in);
+			in.add(nvar);
 		}
 		
-		return in;
+		return new Pair<Stmt,HashSet<String>>(stmt,in);
 	}
 	
 	private void checkUses(HashSet<String> uses,
-			IntersectionFlowSet<String> in, SyntacticElement elem) {
+			HashSet<String> in, SyntacticElement elem) {
 		for (String v : uses) {
 			if (!in.contains(v)) {
 				syntaxError("variable " + v + " might not be initialised",
@@ -130,10 +105,10 @@ public class DefiniteAssignment extends
 		}
 	}
 	
-	public IntersectionFlowSet<String> transfer(boolean branch, Stmt stmt,
-			IntersectionFlowSet<String> in) {
+	public Triple<Stmt, HashSet<String>, HashSet<String>> transfer(
+			Code.IfGoto igoto, Stmt stmt, HashSet<String> in) {
 		HashSet<String> uses = Code.usedVariables(stmt.code);		
 		checkUses(uses,in,stmt);
-		return in;
+		return new Triple(stmt,in,in);
 	}
 }
