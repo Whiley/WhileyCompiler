6b7d5889838ad57f368d5391fee8ef5930c31654
DavePearce <djp@ecs.vuw.ac.nz>
2011-01-24 13:13:29 +1300

    Thinking about how to rework wyone ...

---
+++ src/ext/wyone/core/WConstraint.java
@@ -0,0 +1,12 @@
+package wyone.core;
+
+/**
+ * This class represents a constraint over one or more variables. For example,
+ * "x < 5" is a constraint over variable "x".
+ * 
+ * @author djp
+ * 
+ */
+public interface WConstraint extends WExpr {
+
+}
