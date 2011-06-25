608ad9a098c2b3dc8e41af88fc51d85bd4dbeb0a
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-06-25 17:20:59 +1200

    Added yet another test case.

--- src/wyjc/testing/tests/RuntimeValidTests.java
+++ src/wyjc/testing/tests/RuntimeValidTests.java
@@ -107,6 +107,7 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void FunctionRef_Valid_3_RuntimeTest() { runTest("FunctionRef_Valid_3"); }
  @Test public void FunctionRef_Valid_4_RuntimeTest() { runTest("FunctionRef_Valid_4"); }
  @Test public void FunctionRef_Valid_5_RuntimeTest() { runTest("FunctionRef_Valid_5"); }
+ @Test public void FunctionRef_Valid_6_RuntimeTest() { runTest("FunctionRef_Valid_6"); }
  @Test public void IfElse_Valid_1_RuntimeTest() { runTest("IfElse_Valid_1"); }
  @Test public void IfElse_Valid_2_RuntimeTest() { runTest("IfElse_Valid_2"); }
  @Test public void IfElse_Valid_3_RuntimeTest() { runTest("IfElse_Valid_3"); }
