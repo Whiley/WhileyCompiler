32ed97f9a13f1664828028238cc31e33664a311d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-06-30 14:09:31 +1200

    Added test case, but it doesn't trigger my problem.

--- src/wyjc/testing/tests/RuntimeValidTests.java
+++ src/wyjc/testing/tests/RuntimeValidTests.java
@@ -261,6 +261,7 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void Switch_Valid_4_RuntimeTest() { runTest("Switch_Valid_4"); }
  @Test public void Switch_Valid_5_RuntimeTest() { runTest("Switch_Valid_5"); }
  @Test public void Switch_Valid_6_RuntimeTest() { runTest("Switch_Valid_6"); }
+ @Test public void Switch_Valid_7_RuntimeTest() { runTest("Switch_Valid_7"); }
  @Test public void TryCatch_Valid_1_RuntimeTest() { runTest("TryCatch_Valid_1"); }
  @Test public void TupleType_Valid_1_RuntimeTest() { runTest("TupleType_Valid_1"); }
  @Test public void TupleType_Valid_2_RuntimeTest() { runTest("TupleType_Valid_2"); }
