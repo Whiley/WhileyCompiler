cbb9bebe21da32163cd07492419d55d000243664
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-07-29 16:37:38 +1200

    Added another test case which comes from the matrixMult benchmark.

--- src/wyjc/testing/tests/RuntimeValidTests.java
+++ src/wyjc/testing/tests/RuntimeValidTests.java
@@ -198,7 +198,8 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void Process_Valid_5_RuntimeTest() { runTest("Process_Valid_5"); }
  @Test public void Process_Valid_6_RuntimeTest() { runTest("Process_Valid_6"); }
  @Test public void Process_Valid_7_RuntimeTest() { runTest("Process_Valid_7"); }
- @Test public void Process_Valid_8_RuntimeTest() { runTest("Process_Valid_8"); } 
+ @Test public void Process_Valid_8_RuntimeTest() { runTest("Process_Valid_8"); }
+ @Test public void Process_Valid_9_RuntimeTest() { runTest("Process_Valid_9"); }
  @Test public void RealConst_Valid_1_RuntimeTest() { runTest("RealConst_Valid_1"); }
  @Test public void RealDiv_Valid_1_RuntimeTest() { runTest("RealDiv_Valid_1"); }
  @Test public void RealDiv_Valid_2_RuntimeTest() { runTest("RealDiv_Valid_2"); }
