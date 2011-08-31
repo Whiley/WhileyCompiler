7be6a60c83fe3d9e6292f5e35a1b780a584fa489
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-08-31 16:45:32 +1200

    Building up to properly testing the new automata library.

---
+++ src/wyts/util/Model.java
@@ -0,0 +1,42 @@
+package wyts.util;
+
+import java.util.*;
+
+public class Model<T> {
+	private final ArrayList<T> model;
+	
+	private Model(ArrayList<T> seedValues) {
+		this.model = seedValues;
+	}
+	
+	public int size() {
+		return model.size();
+	}
+	
+	public T get(int i) {
+		return model.get(i);
+	}
+		
+	/**
+	 * An instance of Config determines the necessary parameters of the model.
+	 * 
+	 * @author djp
+	 * 
+	 */
+	public static class KindInfo {
+		/**
+		 * Determine whether this kind is non-sequential or not.
+		 */
+		public boolean NOSEQUENTIAL;
+
+		/**
+		 * Determine minimum number of children this kind can have.
+		 */
+		public int MIN_CHILDREN;
+		
+		/**
+		 * Determine maximum number of children this kind can have.
+		 */
+		public int MAX_CHILDREN;				
+	}	
+}
