06aa45faed09427c218dbd6ef81c17afd8915f2e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-04 10:18:17 +1200

    Well, the whole thing compiles now.  But, the subtype operators are completely broken, there's no simplification and Automatas don't work properly with supplementary data.  But, aside from that, everything is dandy!!!

--- src/wyil/lang/CoerciveSubtypeOperator.java
+++ src/wyil/lang/CoerciveSubtypeOperator.java
@@ -1,45 +1,22 @@
 package wyil.lang;
 
-import static wyautl.lang.Node.K_ANY;
-import static wyautl.lang.Node.K_CHAR;
-import static wyautl.lang.Node.K_DICTIONARY;
-import static wyautl.lang.Node.K_INT;
-import static wyautl.lang.Node.K_LIST;
-import static wyautl.lang.Node.K_RATIONAL;
-import static wyautl.lang.Node.K_SET;
-import static wyautl.lang.Node.K_STRING;
+import static wyil.lang.Type.*;
+import wyautl.lang.Automata;
 import wyil.util.Pair;
 
 
 public class CoerciveSubtypeOperator extends DefaultSubtypeOperator {
 	
-	public CoerciveSubtypeOperator(State[] fromGraph, State[] toGraph) {
-		super(fromGraph,toGraph);
+	public CoerciveSubtypeOperator(Automata fromAutomata, Automata toAutomata) {
+		super(fromAutomata,toAutomata);
 	}
 	
-	public boolean isSubType(int from, int to) {
+	public boolean isRelated(int from, int to) {
+		return super.isRelated(from, to);
+		/*
 		State fromNode = fromGraph[from];
 		State toNode = toGraph[to];	
-		
-		/*
-		if(fromNode.kind == K_RECORD && toNode.kind == K_RECORD) {
-			// labeled nary nodes
-			Pair<String, Integer>[] fields1 = (Pair<String, Integer>[]) fromNode.data;
-			Pair<String, Integer>[] _fields2 = (Pair<String, Integer>[]) toNode.data;				
-			HashMap<String,Integer> fields2 = new HashMap<String,Integer>();
-			for(Pair<String,Integer> f : _fields2) {
-				fields2.put(f.first(), f.second());
-			}
-			for (int i = 0; i != fields1.length; ++i) {
-				Pair<String, Integer> e1 = fields1[i];
-				Integer e2 = fields2.get(e1.first());
-				if (e2 == null || !assumptions.isSubtype(e1.second(),e2)) {
-					return false;
-				}
-			}					
-			return true;
-		} else 
-		*/
+			
 		if(fromNode.kind == K_CHAR && toNode.kind == K_INT) {
 			// ints can flow into chars
 			return true;
@@ -63,50 +40,6 @@ public class CoerciveSubtypeOperator extends DefaultSubtypeOperator {
 		} else {
 			return super.isSubType(from,to);
 		}
-	}
-	
-	public boolean isSuperType(int from, int to) {
-		State fromNode = fromGraph[from];
-		State toNode = toGraph[to];	
-		
-		/*
-		if(fromNode.kind == K_RECORD && toNode.kind == K_RECORD) {
-			// labeled nary nodes
-			Pair<String, Integer>[] _fields1 = (Pair<String, Integer>[]) fromNode.data;
-			Pair<String, Integer>[] fields2 = (Pair<String, Integer>[]) toNode.data;				
-			HashMap<String,Integer> fields1 = new HashMap<String,Integer>();
-			for(Pair<String,Integer> f : _fields1) {
-				fields1.put(f.first(), f.second());
-			}
-			for (int i = 0; i != fields2.length; ++i) {
-				Pair<String, Integer> e2 = fields2[i];
-				Integer e1 = fields1.get(e2.first());
-				if (e1 == null || !assumptions.isSupertype(e1,e2.second())) {
-					return false;
-				}
-			}					
-			return true;					
-
-		} else 
-		*/	
-		if(fromNode.kind == K_CHAR && (toNode.kind == K_RATIONAL || toNode.kind == K_INT)) {
-			// char can flow into int or rational
-			return true;
-		} else if(fromNode.kind == K_INT && (toNode.kind == K_RATIONAL || toNode.kind == K_CHAR)) {
-			// int can flow into rational or char
-			return true;
-		} else if(fromNode.kind == K_LIST && toNode.kind == K_SET) {
-			return assumptions.isSuperSet((Integer) fromNode.data,(Integer) toNode.data);
-		} else if(fromNode.kind == K_LIST && toNode.kind == K_DICTIONARY) {
-			Pair<Integer, Integer> p2 = (Pair<Integer, Integer>) toNode.data;
-			return toGraph[p2.first()].kind == K_INT
-					&& assumptions.isSuperSet((Integer)fromNode.data,p2.second());								
-		} else if(fromNode.kind == K_STRING && toNode.kind == K_LIST) {
-			Integer p2 = (Integer) toNode.data;
-			// TO DO: this is a bug here for cases when the element type is e.g. int|real
-			return toGraph[p2].kind == K_ANY || toGraph[p2].kind == K_INT || toGraph[p2].kind == K_RATIONAL;
-		} else {
-			return super.isSuperType(from, to);
-		}
-	}
+		*/
+	}	
 }
--- src/wyil/lang/DefaultSubtypeOperator.java
+++ src/wyil/lang/DefaultSubtypeOperator.java
@@ -1,16 +1,34 @@
 package wyil.lang;
 
-import static wyautl.lang.Node.*;
-import wyautl.lang.Relation;
+import static wyil.lang.Type.*;
+import wyautl.lang.*;
 import wyil.util.Pair;
 
 
-public class DefaultSubtypeOperator extends Relation {
-	public DefaultSubtypeOperator(State[] fromGraph, State[] toGraph) {
-		super(fromGraph,toGraph);
+public class DefaultSubtypeOperator implements Relation {
+	private final Automata from;
+	private final Automata to;
+	
+	public DefaultSubtypeOperator(Automata fromGraph, Automata toGraph) {
+		this.from = fromGraph;
+		this.to = toGraph;
 	}
 	
-	public boolean isSubType(int from, int to) {
+	public Automata from() {
+		return from;
+	}
+	
+	public Automata to() {
+		return to;
+	}
+	
+	public boolean update(int from, int to) {
+		return false;
+	}
+	
+	public boolean isRelated(int from, int to) {
+		return false;
+		/*
 		State fromNode = fromGraph[from];
 		State toNode = toGraph[to];	
 		
@@ -131,131 +149,6 @@ public class DefaultSubtypeOperator extends Relation {
 		}
 		
 		return false;
-	}
-	
-	public boolean isSuperType(int from, int to) {
-		State fromNode = fromGraph[from];
-		State toNode = toGraph[to];	
-		
-		if(fromNode.kind == toNode.kind) { 
-			switch(fromNode.kind) {
-			case K_EXISTENTIAL:
-				NameID nid1 = (NameID) fromNode.data;
-				NameID nid2 = (NameID) toNode.data;				
-				return nid1.equals(nid2);
-			case K_SET:
-			case K_LIST:
-			case K_PROCESS: {
-				return assumptions.isSuperSet((Integer) fromNode.data,(Integer) toNode.data);
-			}
-			case K_DICTIONARY: {
-				// binary node
-				Pair<Integer, Integer> p1 = (Pair<Integer, Integer>) fromNode.data;
-				Pair<Integer, Integer> p2 = (Pair<Integer, Integer>) toNode.data;
-				return assumptions.isSuperSet(p1.first(),p2.first()) && assumptions.isSuperSet(p1.second(),p2.second());  					
-			}		
-			case K_TUPLE:  {
-				// nary nodes
-				int[] elems1 = (int[]) fromNode.data;
-				int[] elems2 = (int[]) toNode.data;
-				if(elems1.length != elems2.length){ return false; }
-				for(int i=0;i<elems1.length;++i) {
-					if(!assumptions.isSuperSet(elems1[i],elems2[i])) { return false; }
-				}
-				return true;
-			}
-			case K_METHOD:
-			case K_FUNCTION:  {
-				// nary nodes
-				int[] elems1 = (int[]) fromNode.data;
-				int[] elems2 = (int[]) toNode.data;
-				if(elems1.length != elems2.length){
-					return false;
-				}
-				// Check (optional) receiver value first (which is contravariant)
-				int e1 = elems1[0];
-				int e2 = elems2[0];
-				if((e1 == -1 || e2 == -1) && e1 != e2) {
-					return false;
-				} else if (e1 != -1 && e2 != -1
-						&& !assumptions.isSubSet(e1,e2)) {
-					return false;
-				}
-				// Check return value first (which is covariant)
-				e1 = elems1[1];
-				e2 = elems2[1];
-				if(!assumptions.isSuperSet(e1,e2)) {
-					return false;
-				}
-				// Now, check parameters (which are contra-variant)
-				for(int i=2;i<elems1.length;++i) {
-					e1 = elems1[i];
-					e2 = elems2[i];
-					if(!assumptions.isSubSet(e1,e2)) {
-						return false;
-					}
-				}
-				return true;
-			}
-			case K_RECORD:		
-			{
-				// labeled nary nodes
-				Pair<String, Integer>[] fields1 = (Pair<String, Integer>[]) fromNode.data;
-				Pair<String, Integer>[] fields2 = (Pair<String, Integer>[]) toNode.data;				
-				if(fields1.length != fields2.length) {
-					return false;
-				}
-				for (int i = 0; i != fields2.length; ++i) {
-					Pair<String, Integer> e1 = fields1[i];
-					Pair<String, Integer> e2 = fields2[i];						
-						if (!e1.first().equals(e2.first())
-								|| !assumptions.isSuperSet(e1.second(),
-										e2.second())) {
-							return false;
-						}
-				}					
-				return true;					
-			} 		
-			case K_UNION: {														
-				int[] bounds1 = (int[]) toNode.data;		
-
-				// check every bound in c1 is a subtype of some bound in toNode.
-				for(int i : bounds1) {				
-					if(!assumptions.isSuperSet(i,to)) {
-						return false;
-					}								
-				}
-				return true;
-			}									
-			case K_LABEL:
-				throw new IllegalArgumentException("attempting to minimise open recurisve type");		
-			default:
-				// primitive types true immediately
-				return true;
-			}		
-		} else if(fromNode.kind == K_VOID || toNode.kind == K_ANY) {
-			return true;
-		} else if(fromNode.kind == K_UNION) {
-			int[] bounds1 = (int[]) fromNode.data;		
-
-			// check every bound in c1 is a subtype of some bound in c2.
-			for(int i : bounds1) {				
-				if(!assumptions.isSuperSet(i,to)) {
-					return false;
-				}								
-			}
-			return true;	
-		} else if(toNode.kind == K_UNION) {
-			int[] bounds2 = (int[]) toNode.data;		
-
-			// check some bound in c1 is a subtype of some bound in c2.
-			for(int j : bounds2) {				
-				if(assumptions.isSuperSet(from,j)) {
-					return true;
-				}								
-			}				
-		}						
-		
-		return false;
-	}
+		*/
+	}	
 }
