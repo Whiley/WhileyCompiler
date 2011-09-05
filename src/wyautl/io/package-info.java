cd9d6db9818fb8cce3cb2bb3392832dd3a31c3a7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-05 13:25:15 +1200

    More little changes to the documentation.

---
+++ src/wyautl/io/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes for reading and writing automatas via input/output streams.
+ */
+package wyautl.io;
---
+++ src/wyautl/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <b>The Whiley Automata Library</b>.  This library is used for
+ * representing and manipulating finite tree automata.  These automata
+ * are used as the underlying representation of types in Whiley, and
+ * there are a number of relatively complex algorithms required to
+ * manipulate them.  For example, for representing Provides classes for
+ * reading and writing automatas via input/output streams.
+ */
+package wyautl;
