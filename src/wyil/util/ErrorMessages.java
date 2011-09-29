5688e12fa8136339e05c93a9d3699cfe4f92b87d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-09-30 12:11:24 +1300

    Added a class for collating error messages.

---
+++ src/wyil/util/ErrorMessages.java
@@ -0,0 +1,25 @@
+package wyil.util;
+
+/**
+ * <p>
+ * Contains global constants mapping syntax errors to their error messages. The
+ * purpose of this class is bring all syntax error messages into one place, such
+ * that:
+ * </p>
+ * <ol>
+ * <li>It's easy to change error messages, and be sure the changes are applied
+ * universally.</li>
+ * <li>It's easy to reflect on the set of all error messages, and perhaps find
+ * ways to improve them.</li>
+ * </ol>
+ * <p>
+ * In short, error message reporting is done poorly by modern compilers and it
+ * would be nice to do a better job!
+ * </p>
+ * 
+ * @author djp
+ * 
+ */
+public class ErrorMessages {
+
+}
