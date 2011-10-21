11fbe8b0d64ac06985ef841314a31e121ad7298b
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-10-21 18:31:07 +1300

    Added test case for scoping problem with import statements.

--- src/wyjc/testing/tests/RuntimeValidTests.java
+++ src/wyjc/testing/tests/RuntimeValidTests.java
@@ -147,6 +147,7 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void IfElse_Valid_4_RuntimeTest() { runTest("IfElse_Valid_4"); }
  @Test public void Import_Valid_1_RuntimeTest() { runTest("Import_Valid_1"); }
  @Test public void Import_Valid_2_RuntimeTest() { runTest("Import_Valid_2"); } 
+ @Test public void Import_Valid_3_RuntimeTest() { runTest("Import_Valid_3"); }
  @Test public void Interface_Valid_1_RuntimeTest() { runTest("Interface_Valid_1"); }
  @Test public void Intersection_Valid_1_RuntimeTest() { runTest("Intersection_Valid_1"); }
  @Test public void Intersection_Valid_2_RuntimeTest() { runTest("Intersection_Valid_2"); }
---
+++ tests/base/valid/Import_Valid_3.whiley
@@ -0,0 +1,13 @@
+import * from whiley.lang.System
+import * from whiley.lang.String
+
+int toInt([int] ls):
+    r = 0
+    for i in ls:
+        r = r + i
+    return r
+
+public void ::main(System sys, [string] args):
+    ls = [1,2,3,4]
+    s = String.str(toInt(ls))
+    sys.out.println(s)
