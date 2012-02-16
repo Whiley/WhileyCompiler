78c9896d8271a4af95ef02c69317a0aa892056fe
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-02-16 17:15:04 +1300

    Added yet another test case.

--- src/wyjc/testing/tests/RuntimeValidTests.java
+++ src/wyjc/testing/tests/RuntimeValidTests.java
@@ -80,6 +80,7 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void Complex_Valid_1_RuntimeTest() { runTest("Complex_Valid_1"); }
  @Test public void Complex_Valid_2_RuntimeTest() { runTest("Complex_Valid_2"); }
  @Ignore("Known Issue") @Test public void Constant_Valid_1_RuntimeTest() { runTest("Constant_Valid_1"); }
+ @Test public void Constant_Valid_2_RuntimeTest() { runTest("Constant_Valid_2"); }
  @Test public void ConstrainedInt_Valid_1_RuntimeTest() { runTest("ConstrainedInt_Valid_1"); }
  @Test public void ConstrainedInt_Valid_2_RuntimeTest() { runTest("ConstrainedInt_Valid_2"); }
  @Test public void ConstrainedInt_Valid_3_RuntimeTest() { runTest("ConstrainedInt_Valid_3"); }
