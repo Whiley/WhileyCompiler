3d804d8ade835e3866e9ea7be716ba4ae976a3bf
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-02-22 13:02:07 +1300

    Working on the notion of a WhileyProject.  It's a tricky thing though.

--- src/wyc/lang/WhileyProject.java
+++ src/wyc/lang/WhileyProject.java
@@ -17,22 +17,48 @@ import wyil.util.path.Path;
  */
 public final class WhileyProject implements Iterable<WhileyFile> {	
 	/**
-	 * The source path is a list of locations which must be searched in
-	 * ascending order for whiley files.
+	 * The source soots are locations which may contain the root of a package
+	 * structure containing source files.
 	 */
-	private ArrayList<Path.Root> sourcepath;
+	private ArrayList<Path.Root> srcRoots;
 	
 	/**
-	 * The whiley path is a list of locations which must be searched in
-	 * ascending order for wyil files.
+	 * The library roots represent external libraries required for compiling
+	 * this project. They may exist in several forms, including as jar files or
+	 * directory roots (for binary files).
 	 */
-	private ArrayList<Path.Root> whileypath;	
+	private ArrayList<Path.Root> libRoots;	
 	
 	/**
 	 * A cache of those source files which have already been loaded.
 	 */
 	private final HashMap<ModuleID,WhileyFile> srcFileCache = new HashMap<ModuleID,WhileyFile>();
-		
+	
+	public WhileyProject(Collection<Path.Root> srcRoots, Collection<Path.Root> libRoots) {
+		this.srcRoots = new ArrayList<Path.Root>(srcRoots);
+		this.libRoots = new ArrayList<Path.Root>(libRoots);
+	}
+	
+	/**
+	 * Determine the complete set of files that need to be recompiled. This
+	 * includes all source files either out a binary equivalent, or whose
+	 * modification time is more recent. Furthermore, it additionally includes
+	 * all dependents for those files, and their dependents, etc.
+	 * 
+	 * @return
+	 */
+	public List<Path.Entry> determineDelta() {
+		ArrayList<Path.Entry> delta = new ArrayList<Path.Entry>();
+		for(Path.Root srcRoot : srcRoots) {
+			for(Path.Entry e : srcRoot.list()) {
+				if(e.suffix().equals("whiley")) {
+					delta.add(e);
+				}
+			}
+		}
+		return delta;
+	}
+	
 	public Iterator<WhileyFile> iterator() {
 		return srcFileCache.values().iterator();
 	}
