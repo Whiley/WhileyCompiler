5f2d82328faf4a8383910e669b04183ba41f40ae
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-02-23 17:03:31 +1300

    Hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- src/wyc/util/path/ContentType.java
+++ src/wyc/util/path/ContentType.java
@@ -1,5 +1,15 @@
 package wyc.util.path;
 
 public interface ContentType<T extends Path.Entry> {
-	public T accept(Path.Entry e);			
+	public T accept(Path.Entry e);		
+	
+	public static ContentType<Path.SourceEntry> WHILEY_FILE = new ContentType<Path.SourceEntry>() {
+		public Path.SourceEntry accept(Path.Entry e) {
+			if(e.suffix() == "whiley") {
+				return (Path.SourceEntry) e;
+			} else {
+				return null;
+			}
+		}
+	};	
 }
