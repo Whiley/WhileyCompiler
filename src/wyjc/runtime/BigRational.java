df4d3fc24dde377961eede35277d2503d8f93ef7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-03-17 21:29:13 +1300

    Making progress on big rational valueOf.

--- src/wyjc/runtime/BigRational.java
+++ src/wyjc/runtime/BigRational.java
@@ -396,7 +396,13 @@ public final class BigRational extends Number implements Comparable<BigRational>
 	
 
 	public static BigRational valueOf(double d) {
-		long l = Double.doubleToRawLongBits(d);
+		long l = Double.doubleToLongBits(d);
+		// Check against infinities and NAN
+		if (l == 0x7ff0000000000000L || l == 0xfff0000000000000L
+				|| l == 0x7ff8000000000000L) {
+			throw new IllegalArgumentException(
+					"BigInteger.valueOf() --- parameter cannot be infinity or NAN");
+		}
 		// Pull out IEEE754 info
 		boolean sign = (l&0x8000000000000000L) != 0;
 		int exponent = (int) ((l & 0x7FF0000000000000L) >> 52);		
