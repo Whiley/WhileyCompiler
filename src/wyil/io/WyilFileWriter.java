36f12e687d1d559500d3ff55c083a07475bd9d01
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-06-17 10:44:47 +0800

    Ok WyilFileWriter compiles now ... but it's output will be dubious.

--- src/wyil/io/WyilFileWriter.java
+++ src/wyil/io/WyilFileWriter.java
@@ -243,15 +243,12 @@ public final class WyilFileWriter implements Transform {
 			} else {
 				line = "end";
 			}
-		} else if(c instanceof Code.Store && !writeSlots){
-			Code.Store store = (Code.Store) c;
-			line = "store " + getLocal(store.slot,locals) + " : " + store.type;  
-		} else if(c instanceof Code.Load && !writeSlots){
-			Code.Load load = (Code.Load) c;
-			line = "load " + getLocal(load.indexOperand,locals) + " : " + load.type;
+		} else if(c instanceof Code.Copy && !writeSlots){
+			Code.Copy copy = (Code.Copy) c;
+			line = "copy " + getLocal(copy.operand,locals) + " : " + copy.type;
 		} else if(c instanceof Code.Move && !writeSlots){
 			Code.Move move = (Code.Move) c;
-			line = "move " + getLocal(move.slot,locals) + " : " + move.type;
+			line = "move " + getLocal(move.operand,locals) + " : " + move.type;
 		} else if(c instanceof Code.Update && !writeSlots){
 			Code.Update store = (Code.Update) c;
 			String fs = store.fields.isEmpty() ? "" : " ";
@@ -265,18 +262,15 @@ public final class WyilFileWriter implements Transform {
 			}
 			line = "update " + getLocal(store.slot,locals) + " #" + store.level + fs + " : " + store.beforeType + " => " + store.afterType;
 		} else if(c instanceof Code.IfType && !writeSlots){
-			Code.IfType iftype = (Code.IfType) c;
-			if(iftype.slot >= 0) {
-				line = "if " + getLocal(iftype.slot,locals) + " is " + iftype.rightOperand
-						+ " goto " + iftype.target + " : " + iftype.type;
-			} else {
-				line = c.toString();
-			}			
+			Code.IfType iftype = (Code.IfType) c;			
+			line = "if " + getLocal(iftype.leftOperand, locals) + " is "
+					+ iftype.rightOperand + " goto " + iftype.target + " : "
+					+ iftype.type;
 		} else if(c instanceof Code.ForAll && !writeSlots){
 			Code.ForAll fall = (Code.ForAll) c;			
 			String modifies = "";
 			boolean firstTime=true;
-			for(int slot : fall.modifies) {
+			for(int slot : fall.modifiedOperands) {
 				if(!firstTime) {
 					modifies +=", ";
 				}
