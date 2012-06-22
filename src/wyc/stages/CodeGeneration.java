2a6d79adb80c4d33354fc5ae293a32e9ee8c158e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-06-22 17:38:26 +1200

    Making progress ... sort of.

--- src/wyc/stages/CodeGeneration.java
+++ src/wyc/stages/CodeGeneration.java
@@ -354,7 +354,7 @@ public final class CodeGeneration {
 			for (int i = 0; i != fields.size(); ++i) {
 				Expr.AssignedVariable v = (Expr.AssignedVariable) fields.get(i);
 				blk.append(Code.TupleLoad(
-						(Type.EffectiveTuple) v.afterType.raw(),
+						(Type.EffectiveTuple) tg.type.raw(),
 						environment.get(v.var), freeRegister, i), attributes(s));
 			}
 			return blk;
