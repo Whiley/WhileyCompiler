1b444756b5295c1d8becd8ad787f887b079e90f5
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-09-02 11:43:41 -0600

    Ok, that's got it compiling now.

--- modules/wyil/src/wyone/spec/TypeInference.java
+++ modules/wyil/src/wyone/spec/TypeInference.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import wyone.core.Attribute;
 import wyone.core.Type;
-import wyone.spec.*;
 import wyone.util.*;
 import static wyone.util.SyntaxError.*;
 
@@ -54,68 +53,68 @@ public class TypeInference {
 		}
 	}
 
-	protected Type resolve(Expr code, HashMap<String,Type> environment) {
+	protected Type resolve(Expr expr, HashMap<String,Type> environment) {
 		try {
 			Type result;
-			if (code instanceof Expr.Constant) {
-				result = resolve((Expr.Constant) code, environment);
-			} else if (code instanceof Expr.UnOp) {
-				result = resolve((Expr.UnOp) code, environment);
-			} else if (code instanceof Expr.BinOp) {
-				result = resolve((Expr.BinOp) code, environment);
-			} else if (code instanceof Expr.NaryOp) {
-				result = resolve((Expr.NaryOp) code, environment);
-			} else if (code instanceof Expr.Constructor) {
-				result = resolve((Expr.Constructor) code, environment);
+			if (expr instanceof Expr.Constant) {
+				result = resolve((Expr.Constant) expr, environment);
+			} else if (expr instanceof Expr.UnOp) {
+				result = resolve((Expr.UnOp) expr, environment);
+			} else if (expr instanceof Expr.BinOp) {
+				result = resolve((Expr.BinOp) expr, environment);
+			} else if (expr instanceof Expr.NaryOp) {
+				result = resolve((Expr.NaryOp) expr, environment);
+			} else if (expr instanceof Expr.Constructor) {
+				result = resolve((Expr.Constructor) expr, environment);
 			} else {
-				syntaxError("unknown code encountered", filename, code);
+				syntaxError("unknown code encountered", filename, expr);
 				return null;
 			}
-			code.attributes().add(new Attribute.Type(result));
+			expr.attributes().add(new Attribute.Type(result));
 			return result; // dead code
 		} catch (SyntaxError se) {
 			throw se;
 		} catch (Exception ex) {
-			syntaxError("internal failure", filename, code, ex);
+			syntaxError("internal failure", filename, expr, ex);
 		}
 		return null; // dead code
 	}
 
-	protected Type resolve(Expr.Constant code, HashMap<String,Type> environment) {
-		Object v = code.value;
-		Type result;
+	protected Type resolve(Expr.Constant expr, HashMap<String, Type> environment) {
+		Object v = expr.value;
 		if (v instanceof Boolean) {
-			result = Type.T_BOOL;
+			return Type.T_BOOL;
 		} else if (v instanceof Integer) {
-			result = Type.T_INT;
+			return Type.T_INT;
 		} else if (v instanceof Double) {
-			result = Type.T_REAL;
+			return Type.T_REAL;
 		} else if (v instanceof String) {
-			result = Type.T_STRING;
+			return Type.T_STRING;
 		} else {
 			syntaxError("unknown constant encountered ("
-					+ v.getClass().getName() + ")", filename, code);
-			return; // deadcode
+					+ v.getClass().getName() + ")", filename, expr);
+			return null; // deadcode
 		}
-		environment.set(code.target, result);
 	}
 
 	protected Type resolve(Expr.Constructor code, HashMap<String,Type> environment) {
 	
-		if(code.operand != -1) {
-			Type arg_t = environment.get(code.operand);			
-			// TODO: type check parameter argument!
+		if(code.argument != null) {
+			Type arg_t = environment.get(code.argument);
+			
+			// TODO: type check parameter argument
 		}
 
 		Type.Term type = terms.get(code.name);
 		if (type == null) {
 			syntaxError("function not declared", filename, code);
 		}
-		environment.set(code.target,type);
+		
+		return type;
 	}
 
 	protected Type resolve(Expr.UnOp uop, HashMap<String,Type> environment) {		
-		Type t = environment.get(uop.operand);
+		Type t = environment.get(uop.mhs);
 		switch (uop.op) {
 		case LENGTHOF:
 			checkSubtype(Type.T_COMPOUNDANY, t, uop);
@@ -130,7 +129,8 @@ public class TypeInference {
 		default:
 			syntaxError("unknown unary expression encountered", filename, uop);
 		}
-		environment.set(uop.target, t);
+		
+		return t;
 	}
 
 	protected Type resolve(Expr.BinOp bop, HashMap<String,Type> environment) {
@@ -138,8 +138,7 @@ public class TypeInference {
 		Type lhs_t = environment.get(bop.lhs);
 		Type rhs_t = environment.get(bop.rhs);
 		Type result;
-		// FIXME: really need to add coercions somehow
-
+		
 		switch (bop.op) {
 		case ADD: {
 			checkSubtype(Type.T_REAL, lhs_t, bop);
@@ -209,9 +208,9 @@ public class TypeInference {
 		}
 		default:
 			syntaxError("unknown binary expression encountered", filename, bop);
-			return; // dead-code
+			return null; // dead-code
 		}
-		environment.set(bop.target,result);
+		return result;
 	}
 
 	public Type[] append(Type head, Type[] tail) {
