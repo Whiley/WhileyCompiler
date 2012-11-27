cda788f5f7c4ccd04a6b7c157c7f8bbbb282b272
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-11-27 13:41:01 +1300

    WYONE: fixed up licensing for the WYONE module.

--- modules/wyone/src/wyone/io/SpecParser.java
+++ modules/wyone/src/wyone/io/SpecParser.java
@@ -1,3 +1,28 @@
+// Copyright (c) 2011, David J. Pearce (djp@ecs.vuw.ac.nz)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//    * Neither the name of the <organization> nor the
+//      names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL DAVID J. PEARCE BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package wyone.io;
 
 import java.math.BigInteger;
@@ -655,20 +680,29 @@ public class SpecParser {
 		ArrayList<Expr> exprs = new ArrayList<Expr>();
 		match(LeftSquare.class);
 		skipWhiteSpace(true);
-		boolean firstTime = true;
 		checkNotEof();
 		Token token = tokens.get(index);
+		
+		if(!(token instanceof RightSquare)) {
+			Expr expr = parseCondition();
+			skipWhiteSpace(true);
+			token = tokens.get(index);
+			if(token instanceof Bar) {
+				// comprehension
+				return parseComprehensionRest(start,expr,Expr.COp.LISTCOMP);
+			}
+			exprs.add(expr);
+		}
+		
 		while(!(token instanceof RightSquare)) {
-			if(!firstTime) {
-				match(Comma.class);
-				skipWhiteSpace(true);
-			}			
+			match(Comma.class);
+			skipWhiteSpace(true);					
 			exprs.add(parseCondition());
 			skipWhiteSpace(true);
 			checkNotEof();
 			token = tokens.get(index);
-			firstTime=false;
 		}
+		
 		match(RightSquare.class);
 		return new Expr.NaryOp(Expr.NOp.LISTGEN, exprs, sourceAttr(start,
 				index - 1));
