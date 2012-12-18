dceaa5ec4fbd9a551253f75a638789d06c7e68e4
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-12-19 10:57:27 +1300

    Added test cases to test run files.

--- modules/wyjc/src/wyjc/testing/tests/RuntimeValidTests.java
+++ modules/wyjc/src/wyjc/testing/tests/RuntimeValidTests.java
@@ -203,6 +203,7 @@ public class RuntimeValidTests extends TestHarness {
  @Test public void Lambda_Valid_4_RuntimeTest() { runTest("Lambda_Valid_4"); }
  @Test public void Lambda_Valid_5_RuntimeTest() { runTest("Lambda_Valid_5"); }
  @Test public void Lambda_Valid_6_RuntimeTest() { runTest("Lambda_Valid_6"); }
+ @Test public void Lambda_Valid_7_RuntimeTest() { runTest("Lambda_Valid_7"); }
  @Test public void ListAccess_Valid_1_RuntimeTest() { runTest("ListAccess_Valid_1"); }
  @Test public void ListAccess_Valid_2_RuntimeTest() { runTest("ListAccess_Valid_2"); }
  @Test public void ListAccess_Valid_3_RuntimeTest() { runTest("ListAccess_Valid_3"); }
