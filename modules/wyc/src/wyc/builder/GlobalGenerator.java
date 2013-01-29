4c463123672493f8fb1c2835dce48752b87618b9
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-01-30 10:55:59 +1300

    WYC: bug fix for previous bug fix.  Now it's fixed!!

--- modules/wyc/src/wyc/builder/GlobalGenerator.java
+++ modules/wyc/src/wyc/builder/GlobalGenerator.java
@@ -246,10 +246,15 @@ public class GlobalGenerator {
 			UnresolvedType.Nominal dt = (UnresolvedType.Nominal) t;
 			
 			try {
-				NameID nid = resolver.resolveAsName(dt.names,context);
-				Block blk = new Block(1);
-				blk.append(generate(nid));
-				return blk;
+				NameID nid = resolver.resolveAsName(dt.names,context);				
+				Block other = generate(nid);
+				if(other != null) {
+					Block blk = new Block(1);
+					blk.append(other);
+					return blk;
+				} else {
+					return null;
+				}
 			} catch (ResolveError rex) {
 				syntaxError(rex.getMessage(), context, t, rex);
 				return null;
