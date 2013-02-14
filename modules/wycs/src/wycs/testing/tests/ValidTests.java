1233f4c6e9c3f134d5379f11d8e782b3e1b65655
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-02-14 14:55:08 +1300

    WYCS: started adding lots of test cases.

--- modules/wycs/src/wycs/testing/tests/ValidTests.java
+++ modules/wycs/src/wycs/testing/tests/ValidTests.java
@@ -10,4 +10,13 @@ public class ValidTests extends TestHarness {
 	 }
 	
 	@Test public void Test_Valid_001() { verifyPassTest("test_001"); }
+	@Test public void Test_Valid_002() { verifyPassTest("test_002"); }
+	@Test public void Test_Valid_003() { verifyPassTest("test_003"); }
+	@Test public void Test_Valid_004() { verifyPassTest("test_004"); }	
+	@Test public void Test_Valid_005() { verifyPassTest("test_005"); }	
+	@Test public void Test_Valid_006() { verifyPassTest("test_006"); }	
+	@Test public void Test_Valid_007() { verifyPassTest("test_007"); }
+	@Test public void Test_Valid_008() { verifyPassTest("test_008"); }
+	@Test public void Test_Valid_009() { verifyPassTest("test_009"); }
+	@Test public void Test_Valid_010() { verifyPassTest("test_010"); }
 }
