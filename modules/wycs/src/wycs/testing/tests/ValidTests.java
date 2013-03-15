407c1c3d7410bb495e81807d64756bff94352e86
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-03-15 16:50:08 +1300

    WYCS: making some kind of progress

--- modules/wycs/src/wycs/testing/tests/ValidTests.java
+++ modules/wycs/src/wycs/testing/tests/ValidTests.java
@@ -31,6 +31,7 @@ public class ValidTests extends TestHarness {
 	@Test public void Test_Valid_019() { verifyPassTest("test_019"); }
 	@Test public void Test_Valid_020() { verifyPassTest("test_020"); }
 	@Test public void Test_Valid_021() { verifyPassTest("test_021"); }
+	@Test public void Test_Valid_022() { verifyPassTest("test_022"); }
 	
 	@Test public void Test_Valid_050() { verifyPassTest("test_050"); }
 	@Ignore("Known Issue") @Test public void Test_Valid_051() { verifyPassTest("test_051"); }
