363bbee447849d5f8c3e51f811fdd57b301f3e1e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-04-02 11:20:00 +1300

    WYCS: making progress!!

--- modules/wycs/src/wycs/builder/CodeGeneration.java
+++ modules/wycs/src/wycs/builder/CodeGeneration.java
@@ -50,6 +50,154 @@ public class CodeGeneration {
 	}
 	
 	protected Code generate(Expr e) {
-		return Code.Constant(Value.Bool(true));
+		if (e instanceof Expr.Variable) {
+			return generate((Expr.Variable) e);
+		} else if (e instanceof Expr.Constant) {
+			return generate((Expr.Constant) e);
+		} else if (e instanceof Expr.Unary) {
+			return generate((Expr.Unary) e);
+		} else if (e instanceof Expr.Binary) {
+			return generate((Expr.Binary) e);
+		} else if (e instanceof Expr.Nary) {
+			return generate((Expr.Nary) e);
+		} else if (e instanceof Expr.Quantifier) {
+			return generate((Expr.Quantifier) e);
+		} else if (e instanceof Expr.FunCall) {
+			return generate((Expr.FunCall) e);
+		} else if (e instanceof Expr.Load) {
+			return generate((Expr.Load) e);
+		} else if (e instanceof Expr.IndexOf) {
+			return generate((Expr.IndexOf) e);
+		} else {
+			internalFailure("unknown expression encountered (" + e + ")",
+					filename, e);
+			return null;
+		}
+	}
+	
+	protected Code generate(Expr.Variable v) {
+		
+	}
+	
+	protected Code generate(Expr.Constant v) {
+		return Code.Constant(v.value);
+	}
+	
+	protected Code generate(Expr.Unary e) {
+		SemanticType type = e.attribute(TypeAttribute.class).type;
+		Code operand = generate(e.operand);
+		Code.Op opcode;
+		switch(e.op) {
+		case NEG:
+			opcode = Code.Op.NEG;
+			break;
+		case NOT:
+			opcode = Code.Op.NOT;
+			break;
+		case LENGTHOF:
+			opcode = Code.Op.LENGTH;
+			break;
+		default:
+			internalFailure("unknown unary opcode encountered (" + e + ")",
+					filename, e);
+			return null;
+		}
+		return Code.Unary(type, opcode, operand);
+	}
+	
+	protected Code generate(Expr.Binary e) {
+		SemanticType type = e.attribute(TypeAttribute.class).type;
+		Code lhs = generate(e.leftOperand);
+		Code rhs = generate(e.rightOperand);
+		Code.Op opcode;
+		switch(e.op) {
+		case ADD:
+			opcode = Code.Op.ADD;
+			break;
+		case SUB:
+			opcode = Code.Op.SUB;
+			break;
+		case MUL:
+			opcode = Code.Op.MUL;
+			break;
+		case DIV:
+			opcode = Code.Op.DIV;
+			break;
+		case REM:
+			opcode = Code.Op.REM;
+			break;
+		case EQ:
+			opcode = Code.Op.EQ;
+			break;
+		case NEQ:
+			opcode = Code.Op.NEQ;
+			break;
+//		case IMPLIES:
+//			opcode = Code.Op.NEG;
+//			break;
+//		case IFF:
+//			opcode = Code.Op.NEG;
+//			break;
+		case LT:
+			opcode = Code.Op.LT;
+			break;
+		case LTEQ:
+			opcode = Code.Op.LTEQ;
+			break;
+		case GT: {
+			opcode = Code.Op.LT;
+			Code tmp = lhs;
+			lhs = rhs;
+			rhs = tmp;
+			break;
+		}
+		case GTEQ: {
+			opcode = Code.Op.LTEQ;
+			Code tmp = lhs;
+			lhs = rhs;
+			rhs = tmp;
+			break;
+		}
+		case IN:
+			opcode = Code.Op.IN;
+			break;
+		case SUBSET:
+			opcode = Code.Op.SUBSET;
+			break;
+		case SUBSETEQ:
+			opcode = Code.Op.SUBSETEQ;
+			break;
+		case SUPSET: {
+			opcode = Code.Op.SUBSET;
+			Code tmp = lhs;
+			lhs = rhs;
+			rhs = tmp;
+			break;
+		}
+		case SUPSETEQ: {
+			opcode = Code.Op.SUBSETEQ;
+			Code tmp = lhs;
+			lhs = rhs;
+			rhs = tmp;
+			break;
+		}
+//		case SETUNION:
+//			opcode = Code.Op.NEG;
+//			break;
+//		case SETINTERSECTION:
+//			opcode = Code.Op.NEG;
+//			break;
+//		case SETDIFFERENCE:
+//			opcode = Code.Op.NEG;
+//			break;
+//		case LISTAPPEND:
+//			opcode = Code.Op.NEG;
+//			break;
+		default:
+			internalFailure("unknown unary opcode encountered (" + e + ")",
+					filename, e);
+			return null;
+		}
+		return Code.Binary(type, opcode, lhs, rhs);
 	}
 }
