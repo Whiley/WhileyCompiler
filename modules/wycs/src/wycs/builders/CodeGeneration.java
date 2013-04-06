e9923e28455c5f5b58bd46d50a6b4f32094d5d88
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-04-06 17:04:15 +1300

    WYCS: minor tweaks; added another test case.

--- modules/wycs/src/wycs/builders/CodeGeneration.java
+++ modules/wycs/src/wycs/builders/CodeGeneration.java
@@ -391,6 +391,8 @@ public class CodeGeneration {
 				e.attribute(Attribute.Source.class));
 	}
 	
-	private static final Trie WYCS_CORE_MAP = Trie.ROOT.append("wycs").append("core").append("Map");
-	private static final Trie WYCS_CORE_LIST = Trie.ROOT.append("wycs").append("core").append("List");
+	private static final Trie WYCS_CORE_MAP = Trie.ROOT.append("wycs")
+			.append("core").append("Map");
+	private static final Trie WYCS_CORE_LIST = Trie.ROOT.append("wycs")
+			.append("core").append("List");
 }
