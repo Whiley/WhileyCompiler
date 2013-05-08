af07172f26a98424ea80000802cb9eeed7239b0a
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-05-09 09:54:16 +1200

    WYCS: added yet another test case!!!

--- modules/wycs/src/wycs/testing/tests/ValidTests.java
+++ modules/wycs/src/wycs/testing/tests/ValidTests.java
@@ -35,6 +35,7 @@ public class ValidTests extends TestHarness {
 	@Test public void Test_Arith_28() { verifyPassTest("test_arith_28"); }
 	@Test public void Test_Arith_29() { verifyPassTest("test_arith_29"); }
 	@Test public void Test_Arith_30() { verifyPassTest("test_arith_30"); }
+	@Test public void Test_Arith_31() { verifyPassTest("test_arith_31"); }
 	
 	@Test public void Test_Valid_013() { verifyPassTest("test_013"); }
 	@Test public void Test_Valid_014() { verifyPassTest("test_014"); }
