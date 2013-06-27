0ea910f9edd21a333ee77fe237dcf268b6290be2
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-06-27 14:15:32 +0100

    Working on a simple rewrite system implementation.

--- modules/wyrl/src/wyautl/rewrite/SimpleRewriter.java
+++ modules/wyrl/src/wyautl/rewrite/SimpleRewriter.java
@@ -25,6 +25,9 @@
 
 package wyautl.rewrite;
 
+import wyautl.core.Automata;
+import wyautl.core.Automaton;
+
 /**
  * A naive implementation of <code>RewriteSystem</code> which works correctly,
  * but is not efficient.
@@ -32,6 +35,62 @@ package wyautl.rewrite;
  * @author David J. Pearce
  * 
  */
-public class SimpleRewriter {
+public class SimpleRewriter implements RewriteSystem {
 	
+	/**
+	 * The list of available inference rules.
+	 */
+	private final InferenceRule[] inferences;
+
+	/**
+	 * The list of available reduction rules.
+	 */
+	private final ReductionRule[] reductions;
+		
+	public SimpleRewriter(InferenceRule[] inferences, ReductionRule[] reductions) {
+		this.inferences = inferences;
+		this.reductions = reductions;
+	}
+	
+	public boolean apply(Automaton automaton) {
+		// FIXME: obviously, need to do more here!
+		return reduce(automaton, 0);
+	}
+	
+	private boolean reduce(Automaton automaton, int start) {
+		boolean result = false;
+		boolean changed = true;
+		int[] tmp = new int[automaton.nStates() * 2];
+		
+		while (changed) {
+			changed = false;
+			outer: for (int i = start; i < automaton.nStates(); ++i) {
+				if (automaton.get(i) == null) {
+					continue;
+				}
+
+				for (int j = 0; j != reductions.length; ++j) {
+					ReductionRule rr = reductions[j];
+					Activation a = rr.probe(automaton, i);
+					if (a != null) {
+						changed |= a.apply(automaton);
+						if (changed) {
+							break outer;
+						}
+					}
+				}
+			}
+			if (changed) {
+				if (automaton.nStates() > tmp.length) {
+					tmp = new int[automaton.nStates() * 2];
+				}
+				Automata.eliminateUnreachableStates(automaton, start,
+						automaton.nStates(), tmp);
+				result = true;
+			}
+		}
+
+		automaton.minimise();
+		return result;
+	}
 }
