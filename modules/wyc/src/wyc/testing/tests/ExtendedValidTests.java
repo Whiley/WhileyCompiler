36d3c3efa1eedd80698dc936f3afbb93f7982e74
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-07-30 10:48:28 +1200

    More minor tweaks.

--- modules/wyc/src/wyc/testing/tests/ExtendedValidTests.java
+++ modules/wyc/src/wyc/testing/tests/ExtendedValidTests.java
@@ -187,9 +187,9 @@ public class ExtendedValidTests extends TestHarness {
  @Test public void Subtype_Valid_7_RuntimeTest() { verifyPassTest("Subtype_Valid_7"); }
  @Test public void Subtype_Valid_8_RuntimeTest() { verifyPassTest("Subtype_Valid_8"); }
  @Test public void Subtype_Valid_9_RuntimeTest() { verifyPassTest("Subtype_Valid_9"); }
- //@Ignore("Issue #222") 
+ @Ignore("Issue #222") 
  @Test public void Switch_Valid_1_RuntimeTest() { verifyPassTest("Switch_Valid_1"); }
- //@Ignore("Issue #222") 
+ @Ignore("Issue #222") 
  @Test public void Switch_Valid_2_RuntimeTest() { verifyPassTest("Switch_Valid_2"); }
  @Ignore("Requires Type Test") @Test public void TypeEquals_Valid_1_RuntimeTest() { verifyPassTest("TypeEquals_Valid_1"); }
  @Ignore("Known Issue") @Test public void TypeEquals_Valid_10_RuntimeTest() { verifyPassTest("TypeEquals_Valid_10"); }
