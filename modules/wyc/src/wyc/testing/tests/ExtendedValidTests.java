76a1ff8545aa7bb7789fdb131aeb90816002ab34
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-10-30 16:13:18 -0400

    Added another test case.

--- modules/wyc/src/wyc/testing/tests/ExtendedValidTests.java
+++ modules/wyc/src/wyc/testing/tests/ExtendedValidTests.java
@@ -244,4 +244,5 @@ public class ExtendedValidTests extends TestHarness {
  @Ignore("Unclassified") @Test public void While_Valid_11_RuntimeTest() { verifyPassTest("While_Valid_11"); }
  @Ignore("Unclassified") @Test public void While_Valid_12_RuntimeTest() { verifyPassTest("While_Valid_12"); }
  @Ignore("Unclassified") @Test public void While_Valid_13_RuntimeTest() { verifyPassTest("While_Valid_13"); }
+ @Test public void While_Valid_14_RuntimeTest() { verifyPassTest("While_Valid_14"); }
 }
