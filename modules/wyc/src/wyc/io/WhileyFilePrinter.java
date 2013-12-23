f930f03c4766590618ff13ef6dd6baf0c3d7818a
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-12-23 17:27:20 +1300

    Making useful progress ...

--- modules/wyc/src/wyc/io/WhileyFilePrinter.java
+++ modules/wyc/src/wyc/io/WhileyFilePrinter.java
@@ -1,9 +1,12 @@
 package wyc.io;
 
+import static wyc.lang.WhileyFile.internalFailure;
+
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
+import wyc.lang.Expr;
 import wyc.lang.WhileyFile;
 
 /**
@@ -35,6 +38,8 @@ public class WhileyFilePrinter {
 	public void print(WhileyFile.Declaration decl) {
 		if(decl instanceof WhileyFile.Import) {
 			print((WhileyFile.Import)decl);
+		} else if(decl instanceof WhileyFile.Constant) {
+			print((WhileyFile.Constant)decl);
 		} else {
 			throw new RuntimeException("Unknown construct encountered: "
 					+ decl.getClass().getName());
@@ -48,5 +53,78 @@ public class WhileyFilePrinter {
 			out.print(" from ");
 		}
 		out.println(decl.filter.toString().replace('/','.'));
+	}
+	
+	public void print(WhileyFile.Constant decl) {
+		out.print("define ");
+		out.print(decl.name);
+		out.print(" as ");
+		print(decl.constant);
+		out.println();
+	}
+	
+	public void print(Expr expression) {
+		if (expression instanceof Expr.Constant) {
+			print ((Expr.Constant) expression);
+		} else if (expression instanceof Expr.AbstractVariable) {
+			print ((Expr.AbstractVariable) expression);
+		} else if (expression instanceof Expr.ConstantAccess) {
+			print ((Expr.ConstantAccess) expression);
+		} else if (expression instanceof Expr.Set) {
+			print ((Expr.Set) expression);
+		} else if (expression instanceof Expr.List) {
+			print ((Expr.List) expression);
+		} else if (expression instanceof Expr.SubList) {
+			print ((Expr.SubList) expression);
+		} else if (expression instanceof Expr.SubString) {
+			print ((Expr.SubString) expression);
+		} else if (expression instanceof Expr.BinOp) {
+			print ((Expr.BinOp) expression);
+		} else if (expression instanceof Expr.LengthOf) {
+			print ((Expr.LengthOf) expression);
+		} else if (expression instanceof Expr.Dereference) {
+			print ((Expr.Dereference) expression);
+		} else if (expression instanceof Expr.Convert) {
+			print ((Expr.Convert) expression);
+		} else if (expression instanceof Expr.IndexOf) {
+			print ((Expr.IndexOf) expression);
+		} else if (expression instanceof Expr.UnOp) {
+			print ((Expr.UnOp) expression);
+		} else if (expression instanceof Expr.FunctionCall) {
+			print ((Expr.FunctionCall) expression);
+		} else if (expression instanceof Expr.MethodCall) {
+			print ((Expr.MethodCall) expression);
+		} else if (expression instanceof Expr.IndirectFunctionCall) {
+			print ((Expr.IndirectFunctionCall) expression);
+		} else if (expression instanceof Expr.IndirectMethodCall) {
+			print ((Expr.IndirectMethodCall) expression);
+		} else if (expression instanceof Expr.Comprehension) {
+			print ((Expr.Comprehension) expression);
+		} else if (expression instanceof Expr.RecordAccess) {
+			print ((Expr.RecordAccess) expression);
+		} else if (expression instanceof Expr.Record) {
+			print ((Expr.Record) expression);
+		} else if (expression instanceof Expr.Tuple) {
+			print ((Expr.Tuple) expression);
+		} else if (expression instanceof Expr.Map) {
+			print ((Expr.Map) expression);
+		} else if (expression instanceof Expr.FunctionOrMethod) {
+			print ((Expr.FunctionOrMethod) expression);
+		} else if (expression instanceof Expr.Lambda) {
+			print ((Expr.Lambda) expression);
+		} else if (expression instanceof Expr.New) {
+			print ((Expr.New) expression);
+		} else {
+			// should be dead-code
+			throw new RuntimeException("Unknown expression kind encountered: " + expression.getClass().getName());
+		}
+	}
+	
+	public void print(Expr.Constant c) {
+		out.print(c.value);
+	}
+	
+	public void print(Expr.AbstractVariable v) {
+		out.print(v);
 	}
 }
