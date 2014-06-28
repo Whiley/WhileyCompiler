c4452e620f2f0b621b96eb132d97b9628bc4ae79
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-06-29 10:27:40 +1200

    WyCS: continued working on the new WyAL file parser.

--- modules/wycs/src/wycs/io/NewWyalFileParser.java
+++ modules/wycs/src/wycs/io/NewWyalFileParser.java
@@ -1,5 +1,479 @@
 package wycs.io;
 
-public class NewWyalFileParser {
+import java.util.ArrayList;
+import java.util.List;
 
+import wycs.io.NewWyalFileLexer.Token;
+import wycs.syntax.*;
+import wycc.lang.SyntaxError;
+import wycc.lang.SyntacticElement;
+import wycc.lang.Attribute;
+
+public class NewWyalFileParser {
+	private String filename;
+	private ArrayList<Token> tokens;
+	private int index;
+	
+	public NewWyalFileParser(String filename, List<Token> tokens) {
+		this.filename = filename;
+		this.tokens = new ArrayList<Token>(tokens);
+	}
+
+	/**
+	 * Read a <code>WhileyFile</code> from the token stream. If the stream is
+	 * invalid in some way (e.g. contains a syntax error, etc) then a
+	 * <code>SyntaxError</code> is thrown.
+	 * 
+	 * @return
+	 */
+	public WyalFile read() {
+		
+	}
+	
+	/**
+	 * Match a given token kind, whilst moving passed any whitespace encountered
+	 * inbetween. In the case that meet the end of the stream, or we don't match
+	 * the expected token, then an error is thrown.
+	 * 
+	 * @param kind
+	 * @return
+	 */
+	private Token match(Token.Kind kind) {
+		checkNotEof();
+		Token token = tokens.get(index++);
+		if (token.kind != kind) {
+			syntaxError("expecting \"" + kind + "\" here", token);
+		}
+		return token;
+	}
+
+	/**
+	 * Match a given sequence of tokens, whilst moving passed any whitespace
+	 * encountered inbetween. In the case that meet the end of the stream, or we
+	 * don't match the expected tokens in the expected order, then an error is
+	 * thrown.
+	 * 
+	 * @param operator
+	 * @return
+	 */
+	private Token[] match(Token.Kind... kinds) {
+		Token[] result = new Token[kinds.length];
+		for (int i = 0; i != result.length; ++i) {
+			checkNotEof();
+			Token token = tokens.get(index++);
+			if (token.kind == kinds[i]) {
+				result[i] = token;
+			} else {
+				syntaxError("Expected \"" + kinds[i] + "\" here", token);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Attempt to match a given kind of token with the view that it must
+	 * *eventually* be matched. This differs from <code>tryAndMatch()</code>
+	 * because it calls <code>checkNotEof()</code>. Thus, it is guaranteed to
+	 * skip any whitespace encountered in between. This is safe because we know
+	 * there is a terminating token still to come.
+	 * 
+	 * @param kind
+	 * @return
+	 */
+	private Token eventuallyMatch(Token.Kind kind) {
+		checkNotEof();
+		Token token = tokens.get(index);
+		if (token.kind != kind) {
+			return null;
+		} else {
+			index = index + 1;
+			return token;
+		}
+	}
+
+	/**
+	 * Attempt to match a given token(s), whilst ignoring any whitespace in
+	 * between. Note that, in the case it fails to match, then the index will be
+	 * unchanged. This latter point is important, otherwise we could
+	 * accidentally gobble up some important indentation. If more than one kind
+	 * is provided then this will try to match any of them.
+	 * 
+	 * @param terminated
+	 *            Indicates whether or not this function should be concerned
+	 *            with new lines. The terminated flag indicates whether or not
+	 *            the current construct being parsed is known to be terminated.
+	 *            If so, then we don't need to worry about newlines and can
+	 *            greedily consume them (i.e. since we'll eventually run into
+	 *            the terminating symbol).
+	 * @param kinds
+	 * 
+	 * @return
+	 */
+	private Token tryAndMatch(boolean terminated, Token.Kind... kinds) {
+		// If the construct being parsed is know to be terminated, then we can
+		// skip all whitespace. Otherwise, we can't skip newlines as these are
+		// significant.
+		int next = terminated ? skipWhiteSpace(index) : skipLineSpace(index);
+
+		if (next < tokens.size()) {
+			Token t = tokens.get(next);
+			for (int i = 0; i != kinds.length; ++i) {
+				if (t.kind == kinds[i]) {
+					index = next + 1;
+					return t;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Attempt to match a given token on the *same* line, whilst ignoring any
+	 * whitespace in between. Note that, in the case it fails to match, then the
+	 * index will be unchanged. This latter point is important, otherwise we
+	 * could accidentally gobble up some important indentation.
+	 * 
+	 * @param kind
+	 * @return
+	 */
+	private Token tryAndMatchOnLine(Token.Kind kind) {
+		int next = skipLineSpace(index);
+		if (next < tokens.size()) {
+			Token t = tokens.get(next);
+			if (t.kind == kind) {
+				index = next + 1;
+				return t;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Match a the end of a line. This is required to signal, for example, the
+	 * end of the current statement.
+	 */
+	private void matchEndLine() {
+		// First, parse all whitespace characters except for new lines
+		index = skipLineSpace(index);
+
+		// Second, check whether we've reached the end-of-file (as signaled by
+		// running out of tokens), or we've encountered some token which not a
+		// newline.
+		if (index >= tokens.size()) {
+			return; // EOF
+		} else if (tokens.get(index).kind != Token.Kind.NewLine) {
+			syntaxError("expected end-of-line", tokens.get(index));
+		} else {
+			index = index + 1;
+		}
+	}
+
+	/**
+	 * Check that the End-Of-File has not been reached. This method should be
+	 * called from contexts where we are expecting something to follow.
+	 */
+	private void checkNotEof() {
+		skipWhiteSpace();
+		if (index >= tokens.size()) {
+			throw new SyntaxError("unexpected end-of-file", filename,
+					index - 1, index - 1);
+		}
+	}
+
+	/**
+	 * Skip over any whitespace characters.
+	 */
+	private void skipWhiteSpace() {
+		index = skipWhiteSpace(index);
+	}
+
+	/**
+	 * Skip over any whitespace characters, starting from a given index and
+	 * returning the first index passed any whitespace encountered.
+	 */
+	private int skipWhiteSpace(int index) {
+		while (index < tokens.size() && isWhiteSpace(tokens.get(index))) {
+			index++;
+		}
+		return index;
+	}
+
+	/**
+	 * Skip over any whitespace characters that are permitted on a given line
+	 * (i.e. all except newlines), starting from a given index and returning the
+	 * first index passed any whitespace encountered.
+	 */
+	private int skipLineSpace(int index) {
+		while (index < tokens.size() && isLineSpace(tokens.get(index))) {
+			index++;
+		}
+		return index;
+	}
+
+	/**
+	 * Skip over any empty lines. That is lines which contain only whitespace
+	 * and comments.
+	 */
+	private void skipEmptyLines() {
+		int tmp = index;
+		do {
+			tmp = skipLineSpace(tmp);
+			if (tmp < tokens.size()
+					&& tokens.get(tmp).kind != Token.Kind.NewLine) {
+				return; // done
+			} else if(tmp >= tokens.size()) {
+				index = tmp;
+				return; // end-of-file reached
+			}
+			// otherwise, skip newline and continue
+			tmp = tmp + 1;
+			index = tmp;
+		} while (true);
+		// deadcode
+	}
+
+	/**
+	 * Define what is considered to be whitespace.
+	 * 
+	 * @param token
+	 * @return
+	 */
+	private boolean isWhiteSpace(Token token) {
+		return token.kind == Token.Kind.NewLine || isLineSpace(token);
+	}
+
+	/**
+	 * Define what is considered to be linespace.
+	 * 
+	 * @param token
+	 * @return
+	 */
+	private boolean isLineSpace(Token token) {
+		return token.kind == Token.Kind.Indent
+				|| token.kind == Token.Kind.LineComment
+				|| token.kind == Token.Kind.BlockComment;
+	}
+
+	/**
+	 * Parse a character from a string of the form 'c' or '\c'.
+	 * 
+	 * @param input
+	 * @return
+	 */
+	private char parseCharacter(String input) {
+		int pos = 1;
+		char c = input.charAt(pos++);
+		if (c == '\\') {
+			// escape code
+			switch (input.charAt(pos++)) {
+			case 'b':
+				c = '\b';
+				break;
+			case 't':
+				c = '\t';
+				break;
+			case 'n':
+				c = '\n';
+				break;
+			case 'f':
+				c = '\f';
+				break;
+			case 'r':
+				c = '\r';
+				break;
+			case '"':
+				c = '\"';
+				break;
+			case '\'':
+				c = '\'';
+				break;
+			case '\\':
+				c = '\\';
+				break;			
+			default:
+				throw new RuntimeException("unrecognised escape character");
+			}
+		}
+		return c;
+	}
+
+	/**
+	 * Parse a string whilst interpreting all escape characters.
+	 * 
+	 * @param v
+	 * @return
+	 */
+	protected String parseString(String v) {
+		/*
+		 * Parsing a string requires several steps to be taken. First, we need
+		 * to strip quotes from the ends of the string.
+		 */
+		v = v.substring(1, v.length() - 1);
+		// Second, step through the string and replace escaped characters
+		for (int i = 0; i < v.length(); i++) {
+			if (v.charAt(i) == '\\') {
+				if (v.length() <= i + 1) {
+					throw new RuntimeException("unexpected end-of-string");
+				} else {
+					char replace = 0;
+					int len = 2;
+					switch (v.charAt(i + 1)) {
+					case 'b':
+						replace = '\b';
+						break;
+					case 't':
+						replace = '\t';
+						break;
+					case 'n':
+						replace = '\n';
+						break;
+					case 'f':
+						replace = '\f';
+						break;
+					case 'r':
+						replace = '\r';
+						break;
+					case '"':
+						replace = '\"';
+						break;
+					case '\'':
+						replace = '\'';
+						break;
+					case '\\':
+						replace = '\\';
+						break;
+					case 'u':
+						len = 6; // unicode escapes are six digits long,
+						// including "slash u"
+						String unicode = v.substring(i + 2, i + 6);
+						replace = (char) Integer.parseInt(unicode, 16); // unicode
+						break;
+					default:
+						throw new RuntimeException("unknown escape character");
+					}
+					v = v.substring(0, i) + replace + v.substring(i + len);
+				}
+			}
+		}
+		return v;
+	}
+
+	/**
+	 * Parse a token representing a byte value. Every such token is a sequence
+	 * of one or more binary digits ('0' or '1') followed by 'b'. For example,
+	 * "00110b" is parsed as the byte value 6.
+	 * 
+	 * @param input
+	 *            The token representing the byte value.
+	 * @return
+	 */
+	private byte parseByte(Token input) {
+		String text = input.text;
+		if (text.length() > 9) {
+			syntaxError("invalid binary literal (too long)", input);
+		}
+		int val = 0;
+		for (int i = 0; i != text.length() - 1; ++i) {
+			val = val << 1;
+			char c = text.charAt(i);
+			if (c == '1') {
+				val = val | 1;
+			} else if (c == '0') {
+
+			} else {
+				syntaxError("invalid binary literal (invalid characters)",
+						input);
+			}
+		}
+		return (byte) val;
+	}
+
+	private Attribute.Source sourceAttr(int start, int end) {
+		Token t1 = tokens.get(start);
+		Token t2 = tokens.get(end);
+		// FIXME: problem here with the line numbering ?
+		return new Attribute.Source(t1.start, t2.end(), 0);
+	}
+
+	private void syntaxError(String msg, SyntacticElement e) {
+		Attribute.Source loc = e.attribute(Attribute.Source.class);
+		throw new SyntaxError(msg, filename, loc.start, loc.end);
+	}
+
+	private void syntaxError(String msg, Token t) {
+		throw new SyntaxError(msg, filename, t.start, t.start + t.text.length()
+				- 1);
+	}
+
+	/**
+	 * Represents a given amount of indentation. Specifically, a count of tabs
+	 * and spaces. Observe that the order in which tabs / spaces occurred is not
+	 * retained.
+	 * 
+	 * @author David J. Pearce
+	 * 
+	 */
+	private static class Indent extends Token {
+		private final int countOfSpaces;
+		private final int countOfTabs;
+
+		public Indent(String text, int pos) {
+			super(Token.Kind.Indent, text, pos);
+			// Count the number of spaces and tabs
+			int nSpaces = 0;
+			int nTabs = 0;
+			for (int i = 0; i != text.length(); ++i) {
+				char c = text.charAt(i);
+				switch (c) {
+				case ' ':
+					nSpaces++;
+					break;
+				case '\t':
+					nTabs++;
+					break;
+				default:
+					throw new IllegalArgumentException(
+							"Space or tab character expected");
+				}
+			}
+			countOfSpaces = nSpaces;
+			countOfTabs = nTabs;
+		}
+
+		/**
+		 * Test whether this indentation is considered "less than or equivalent"
+		 * to another indentation. For example, an indentation of 2 spaces is
+		 * considered less than an indentation of 3 spaces, etc.
+		 * 
+		 * @param other
+		 *            The indent to compare against.
+		 * @return
+		 */
+		public boolean lessThanEq(Indent other) {
+			return countOfSpaces <= other.countOfSpaces
+					&& countOfTabs <= other.countOfTabs;
+		}
+
+		/**
+		 * Test whether this indentation is considered "equivalent" to another
+		 * indentation. For example, an indentation of 3 spaces followed by 1
+		 * tab is considered equivalent to an indentation of 1 tab followed by 3
+		 * spaces, etc.
+		 * 
+		 * @param other
+		 *            The indent to compare against.
+		 * @return
+		 */
+		public boolean equivalent(Indent other) {
+			return countOfSpaces == other.countOfSpaces
+					&& countOfTabs == other.countOfTabs;
+		}
+	}
+
+	/**
+	 * An abstract indentation which represents the indentation of top-level
+	 * declarations, such as function declarations. This is used to simplify the
+	 * code for parsing indentation.
+	 */
+	private static final Indent ROOT_INDENT = new Indent("", 0);
 }
