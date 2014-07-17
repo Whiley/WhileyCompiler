36575241f5d08c02ac6ebf49c7594fb52ff56387
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-07-18 10:58:13 +1200

    WyRL: added simplistic mechanism for finding triggers.

--- modules/wyrl/examples/quantifiers/Quantifiers$native.java
+++ modules/wyrl/examples/quantifiers/Quantifiers$native.java
@@ -6,8 +6,70 @@ public class Quantifiers$native {
 	private static final int NULL = Automaton.K_FREE - Quantifiers.K_Null;
 	
 	public static Automaton.Term bind(Automaton automaton, Automaton.List args) {
-		int result = bind(automaton,args.get(0),args.get(1),args.get(2));
+		
+		// TODO: add support for generating multiple bindings (rather than just
+		// returning the first found as we currently do)
+		
+		int result = find(automaton,args.get(0),args.get(1),args.get(2));
 		return (Automaton.Term) automaton.get(result);
+	}
+	
+	/**
+	 * <p>
+	 * Traverse the automaton attempting to find a binding (if one exists). In
+	 * essence, we descend the parameterised expression e2 looking for trigger
+	 * points. These are terms of the same kind as the concrete expression we
+	 * are attempting to bind against. Once a trigger is found, we proceed to
+	 * try and bind against it. If the binding succeeds, this is returned
+	 * immediately. Otherwise, the search continues until all options are
+	 * exhausted.
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>NOTE:</b> this is an expensive operation. To mitigate this, we
+	 * minimise the number of potential trigger points by only descending
+	 * logical operators, but not through general expressions. This limits the
+	 * power of quantifier instantiation, but is a reasonable restriction at
+	 * this stage.
+	 * </p>
+	 * 
+	 * @param automaton
+	 *            The automaton we're traversing.
+	 * @param r1
+	 *            The concrete expression we are binding against
+	 * @param v
+	 *            The variable we're attempting to bind
+	 * @param r2
+	 *            The expression parameterised against v through which are are
+	 *            searching for a trigger to try and bind against.
+	 * @return
+	 */
+	private static int find(Automaton automaton, int r1, int v, int r2) {
+		System.out.println("FINDING: " + r1 + ", " + v + ", " + r2);
+		Automaton.State s1 = automaton.get(r1);
+		Automaton.State s2 = automaton.get(r2);
+		
+		if(s1.kind == s2.kind) {			
+			// This indicates a potential trigger point, so attempt to bind.
+			return bind(automaton,r1,v,r2);
+		} else {
+			// This is not a trigger point. Therefore, if its a logical
+			// expression continue the search through the expression; Otherwise,
+			// give up.
+			switch(s2.kind) {
+			case Quantifiers.K_Not: {
+				Automaton.Term t2 = (Automaton.Term) s2;
+				return find(automaton,r1,v,t2.contents);
+			}				
+			case Quantifiers.K_And:
+			case Quantifiers.K_Or: {
+				// FIXME: implement this!
+			}
+			}
+			
+			// Give up
+			return NULL;			
+		}
 	}
 	
 	/**
@@ -29,8 +91,7 @@ public class Quantifiers$native {
 	 * @return
 	 */
 	private static int bind(Automaton automaton, int r1, int v, int r2) {
-						
-		System.out.println("TRAVERSING: " + r1 + ", " + v + ", " + r2);
+		System.out.println("BINDING: " + r1 + ", " + v + ", " + r2);
 		if(r1 == r2) {
 			// This indicates we've encountered two identical expressions,
 			// neither of which can contain the variable we're binding.
@@ -40,7 +101,7 @@ public class Quantifiers$native {
 			// This indicates we've hit the variable parameter, which is the
 			// success condition. Everything in the concrete expression is thus
 			// matched
-			System.out.println("MATCHED!");
+			System.out.println("*** MATCHED ***");
 			return r1;
 		}
 						
@@ -57,7 +118,6 @@ public class Quantifiers$native {
 			// construction). Therefore, no binding is possible. 
 			return NULL;
 		} else if(s1 instanceof Automaton.Term) {
-			System.out.println("*** TERM");
 			Automaton.Term t1 = (Automaton.Term) s1;
 			Automaton.Term t2 = (Automaton.Term) s2;						
 			// In this case, we have two non-identical terms of the same
@@ -65,7 +125,6 @@ public class Quantifiers$native {
 			// in an effort to complete the binding.
 			return bind(automaton, t1.contents, v, t2.contents);			
 		} else {
-			System.out.println("*** COLLECTION");
 			Automaton.Collection c1 = (Automaton.Collection) s1;
 			Automaton.Collection c2 = (Automaton.Collection) s2;
 			int c1_size = c1.size();
@@ -74,7 +133,6 @@ public class Quantifiers$native {
 				// Here, we have collections of different size and, hence, binding must fail.
 				return NULL;
 			} else if(s1 instanceof Automaton.List){
-				System.out.println("***** LIST");
 				// Lists are the easiest to handle, because we can perform a
 				// linear comparison.
 				Automaton.List l1 = (Automaton.List) c1;
