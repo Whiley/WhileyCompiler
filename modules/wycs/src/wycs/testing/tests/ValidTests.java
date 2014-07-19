b964a1e95ebc88a1b58fd332679524ca99c7153e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-07-19 22:32:20 +1200

    WyCS: Fixed up the syntax for a bunch of test cases.  Found one which now passes, though that was specifically constructed for the problem I just solved!!! :

--- modules/wycs/src/wycs/testing/tests/ValidTests.java
+++ modules/wycs/src/wycs/testing/tests/ValidTests.java
@@ -83,7 +83,7 @@ public class ValidTests extends TestHarness {
 	@Ignore("Known Issue") @Test public void Test_List_2() { verifyPassTest("test_list_02"); }	
 	@Test public void Test_List_4() { verifyPassTest("test_list_04"); }
 	@Test public void Test_List_5() { verifyPassTest("test_list_05"); }	
-	@Ignore("#231,#303") 
+	//@Ignore("#231,#303") 
 	@Test public void Test_List_6() { verifyPassTest("test_list_06"); }
 	@Test public void Test_List_7() { verifyPassTest("test_list_07"); }
 	@Test public void Test_List_8() { verifyPassTest("test_list_08"); }
@@ -92,7 +92,7 @@ public class ValidTests extends TestHarness {
 	@Test public void Test_List_11() { verifyPassTest("test_list_11"); }
 	
 	@Test public void Test_List_12() { verifyPassTest("test_list_12"); }
-	@Ignore("#373") @Test public void Test_List_13() { verifyPassTest("test_list_13"); }
+	@Test public void Test_List_13() { verifyPassTest("test_list_13"); }
 	
 	@Test public void Test_Tuple_1() { verifyPassTest("test_tuple_01"); }
 	
