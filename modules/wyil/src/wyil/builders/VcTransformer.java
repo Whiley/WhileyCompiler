039dc7abf94be0d0573f98791c80092e2ddfb482
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-07-23 17:49:20 +1200

    WyIL: working on #379

--- modules/wyil/src/wyil/builders/VcTransformer.java
+++ modules/wyil/src/wyil/builders/VcTransformer.java
@@ -178,14 +178,15 @@ public class VcTransformer {
 			Expr assertion = buildAssertion(0, implication, uses, branch);
 			wycsFile.add(wycsFile.new Assert(code.msg, assertion, branch
 					.entry().attributes()));
-		} 		
-		// FIXME: the following comment is contested.  See #377
+		} else {
+			// FIXME: the following comment is contested.  See #377
 
-		// We can now safely assume the assertion. Note that we must assume it
-		// here, even for the case where it is already proven. This is because
-		// it is necessary to cut out unreaslizable paths which may continue
-		// after this assertion.
-		branch.add(test);
+			// We can now safely assume the assertion. Note that we must assume it
+			// here, even for the case where it is already proven. This is because
+			// it is necessary to cut out unreaslizable paths which may continue
+			// after this assertion.
+			branch.add(test);
+		}
 	}
 
 	/**
