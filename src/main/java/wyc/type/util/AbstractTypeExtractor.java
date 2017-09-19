6335c1b19e382620a23b90ede6f821a6dfb641e6
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-09-19 14:28:28 +1200

    Relicensed all source files to Apache 2.0
    This completes the relicensing process from BSD to Apache 2.0.

--- src/main/java/wyc/type/util/AbstractTypeExtractor.java
+++ src/main/java/wyc/type/util/AbstractTypeExtractor.java
@@ -1,4 +1,4 @@
-// Copyright 2017 David J. Pearce
+// Copyright 2011 The Whiley Project Developers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
